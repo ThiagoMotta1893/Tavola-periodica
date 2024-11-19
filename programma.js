36689d717b5dcfecb59592829ddfd971f65a22aa
ThiagoMotta1893 <andrea.nicoli@iismontalegenova.it>
2024-11-19 12:39:59 +0100

    tavola periodica

--- programma.js
+++ programma.js
@@ -1,42 +1,34 @@
 
-let idrogeno = {};
 
-idrogeno.simbolo = 'H'
-idrogeno.numero = 1;
-idrogeno.nome = 'idrogeno'
-
-let elio = {};
-
-elio.simbolo = 'HE';
-elio.numero = 2;
-elio.nome = 'Elio'; 
-
-let litio = {};
-
-litio.simbolo = 'li';
-litio.numero = 3;
-litio.nome = 'Litio'; 
-
-let elementi = [idrogeno, elio];
 
 for (const element of elementi) {
     let contenitore = document.getElementById('contenitore'); 
+    contenitore.style.position = 'relative';
 
 
 let cella = document.createElement('div');
 cella.style.borderStyle = 'solid';
-cella.style.width = '70'
+cella.style.width = '70px'
+cella.style.height = '100px'
+cella.style.position = 'absolute'
+cella.style.left = element.xpos * 70 + 'px';
+cella.style.top = element.ypos * 120 + 'px'
+cella.style.backgroundColor = '#' + element['cpk-hex']
 
 let numeroAtomico = document.createElement ('p'); 
-numeroAtomico.innerText = element.numero;
+numeroAtomico.innerText = element.number;
 cella.appendChild(numeroAtomico);
 
 let simbolo = document.createElement('a');
-simbolo.innerText = element.simbolo;
+simbolo.innerText = element.symbol;
+simbolo.href = element.source;
 cella.appendChild(simbolo);
+simbolo.style.fontsize = '24px'
+simbolo.style.fontWeight = 'bold'
+simbolo.style.color = 'black';
 
 let nome = document.createElement('p'); 
-nome.innerText = element.nome;
+nome.innerText = element.name;
 cella.appendChild(nome);
 
 contenitore.appendChild(cella);
